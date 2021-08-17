672b893ac1bd7d6af44dddcc90621a49bc9f8e08
cazaresmr <michaelcazares@gmail.com>
2021-08-17 05:59:12 -0700

    initial commit

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+const plus = (number) => {
+  return (plusNumber) => number + plusNumber;
+};
+
+const plus15 = plus(15);
+
+plus15(1);  // 16
+
+console.log(plus15(10));  // 25
